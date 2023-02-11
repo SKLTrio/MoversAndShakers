31a4333147a5f5bcc882857ab408db7a7a4d3472
Neve Lane <123910307+SKLTrio@users.noreply.github.com>
2023-02-11 16:01:22 +0000

    Update PlayerLocomotion.cs

--- Assets/Player/Scripts/PlayerLocomotion.cs
+++ Assets/Player/Scripts/PlayerLocomotion.cs
@@ -20,7 +20,7 @@ public class PlayerLocomotion : MonoBehaviour
 
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = true;
         characterController = GetComponent<CharacterController>();
         SetCurrentCamera();
     }
